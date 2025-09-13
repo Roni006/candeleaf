9cc58a07bb92f4711978e655d03018a6619b1ea9
Anik Islam <anik54908@gmail.com>
2025-09-14 00:22:10 +0600

    Created navbar product and popular section

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+      boxShadow: {
+        product: "0px 34px 74px rgba(123, 123, 123, 0.2)"
+      }
+     },
+   },
+   plugins: [],
+ }
