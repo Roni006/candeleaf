ed7a4d97219c81ffb8c66da483007504e20f5cdd
Anik Islam <anik54908@gmail.com>
2025-09-15 01:15:19 +0600

    complete the projec

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,12 +1,16 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
       boxShadow: {
         product: "0px 34px 74px rgba(123, 123, 123, 0.2)"
+      },
+      fontFamily: {
+        popins: ["poppins", "sans-serif"]
       }
-     },
-   },
-   plugins: [],
- }
+    },
+     
+  }, 
+plugins: [],
+}
